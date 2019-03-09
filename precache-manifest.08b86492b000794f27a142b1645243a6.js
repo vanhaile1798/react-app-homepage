c6180bca8c51232286de8b84c65c270571710aa7
hailv-0853 <le.van.hai@sun-asterisk.com>
2019-03-09 08:57:19 +0700

    Updates

---
+++ precache-manifest.08b86492b000794f27a142b1645243a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4155ca6470ca749b4ffc",
+    "url": "/react-app-homepage/static/js/runtime~main.4155ca64.js"
+  },
+  {
+    "revision": "7bc31c0acbda8cce7d35",
+    "url": "/react-app-homepage/static/js/main.7bc31c0a.chunk.js"
+  },
+  {
+    "revision": "8713295d49f9a8c4744d",
+    "url": "/react-app-homepage/static/js/2.8713295d.chunk.js"
+  },
+  {
+    "revision": "7bc31c0acbda8cce7d35",
+    "url": "/react-app-homepage/static/css/main.2ee382b5.chunk.css"
+  },
+  {
+    "revision": "93c1cbb7da631103bd4147ec4aced2bc",
+    "url": "/react-app-homepage/index.html"
+  }
+];
