99a692f0fb0cf10e459878fea43aa18dd1584760
hailv-0853 <le.van.hai@sun-asterisk.com>
2019-03-09 09:02:52 +0700

    Updates

--- precache-manifest.e1bfb9088d2f9898f24de5224759562b.js
+++ precache-manifest.e1bfb9088d2f9898f24de5224759562b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-app-homepage/static/js/runtime~main.4155ca64.js"
   },
   {
-    "revision": "5a38b51f937e1e802e94",
-    "url": "/react-app-homepage/static/js/main.5a38b51f.chunk.js"
+    "revision": "653922a6d5933779b476",
+    "url": "/react-app-homepage/static/js/main.653922a6.chunk.js"
   },
   {
     "revision": "8713295d49f9a8c4744d",
     "url": "/react-app-homepage/static/js/2.8713295d.chunk.js"
   },
   {
-    "revision": "5a38b51f937e1e802e94",
+    "revision": "653922a6d5933779b476",
     "url": "/react-app-homepage/static/css/main.2ee382b5.chunk.css"
   },
   {
-    "revision": "962a1bc49e1a7d822585e4a0c1cff0c7",
+    "revision": "2999e47c98ae846a9da83a6f16047a90",
     "url": "/react-app-homepage/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-app-homepage/precache-manifest.8180156c0d9e14010560002cce394a51.js"
+  "/react-app-homepage/precache-manifest.e1bfb9088d2f9898f24de5224759562b.js"
 );
 
 workbox.clientsClaim();
